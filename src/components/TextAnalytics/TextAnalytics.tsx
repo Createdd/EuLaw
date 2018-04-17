a42433b3627a54046794a99ee30eec1d1355a42d
Daniel Deutsch <deudan1010@gmail.com>
2018-04-17 13:45:22 +0200

    Improve life cycle methods

--- src/components/TextAnalytics/TextAnalytics.tsx
+++ src/components/TextAnalytics/TextAnalytics.tsx
@@ -5,7 +5,7 @@ var nlp = require('compromise');
 import { TextAnalyticsPropsType, TextAnalyticsStateType } from './types';
 import TextAnalyticsStyled from './styles';
 
-// import ExampleCase from '../../data/exampleData/exampleCase';
+import ExampleCase from '../../data/exampleData/exampleCase';
 import MemberStates from '../../data/exampleData/memberStates';
 
 class TextAnalytics extends React.Component<
@@ -20,81 +20,18 @@ class TextAnalytics extends React.Component<
     };
   }
 
-  static getDerivedStateFromProps(nextProps: TextAnalyticsPropsType, prevState: TextAnalyticsStateType) {
-    const text = nlp(nextProps.text); // TODO: Use input instead of example
-    // const text = nlp(ExampleCase.text);
-    const topics = text
-      .topics()
-      .slice(0, 4)
-      .out('frequency')
-      .map((top: { normal: string }) => top.normal.toLowerCase());
-    const places = text
-      .nouns()
-      .places()
-      .slice(0, 4)
-      .out('frequency')
-      .map((pl: { normal: string }) => pl.normal.toLowerCase());
-
+  resultsHandler = (): void => {
     const intersect: string[] = _intersection(
       MemberStates.map((str: string) => str.toLowerCase()),
-      places
+      this.state.places
     );
     const newRes: TextAnalyticsStateType = {
       topics: [],
       places: intersect
     };
 
-    nextProps.results(newRes);
-    
-    // If a new text is supplied, update the state
-    if (!_isEqual(prevState, { topics, places })) {
-      return({
-        topics,
-        places
-      });
-    }
-
-    return null;
+    this.props.results(newRes);
   }
-
-  // resultsHandler = (res: TextAnalyticsStateType): void => {
-  //   this.props.results(res);
-  // }
-
-  // calculateResults = (): void => {
-  //   const text = nlp(this.props.text); // TODO: Use input instead of example
-  //   // const text = nlp(ExampleCase.text);
-  //   const topics = text
-  //     .topics()
-  //     .slice(0, 4)
-  //     .out('frequency')
-  //     .map((top: { normal: string }) => top.normal.toLowerCase());
-  //   const places = text
-  //     .nouns()
-  //     .places()
-  //     .slice(0, 4)
-  //     .out('frequency')
-  //     .map((pl: { normal: string }) => pl.normal.toLowerCase());
-
-  //   // If a new text is supplied, update the state
-  //   if (!_isEqual(this.state, { topics, places })) {
-  //     this.setState({
-  //       topics,
-  //       places
-  //     });
-  //   }
-
-  //   const intersect: string[] = _intersection(
-  //     MemberStates.map((str: string) => str.toLowerCase()),
-  //     places
-  //   );
-  //   const newRes: TextAnalyticsStateType = {
-  //     topics: [],
-  //     places: intersect
-  //   };
-
-  //   this.resultsHandler(newRes);
-  // }
 
   renderResults = (): JSX.Element => {
     return (
@@ -109,6 +46,39 @@ class TextAnalytics extends React.Component<
         </li>
       </ul>
     );
+  }
+
+  static getDerivedStateFromProps(
+    nextProps: TextAnalyticsPropsType,
+    prevState: TextAnalyticsStateType
+  ) {
+    // const text = nlp(nextProps.text); // TODO: Use input instead of example
+    const text = nlp(ExampleCase.text);
+    const topics = text
+      .topics()
+      .slice(0, 4)
+      .out('frequency')
+      .map((top: { normal: string }) => top.normal.toLowerCase());
+    const places = text
+      .nouns()
+      .places()
+      .slice(0, 4)
+      .out('frequency')
+      .map((pl: { normal: string }) => pl.normal.toLowerCase());
+
+    // If a new text is supplied, update the state
+    if (!_isEqual(prevState, { topics, places })) {
+      return {
+        topics,
+        places
+      };
+    }
+
+    return null;
+  }
+
+  componentDidUpdate(): void {
+    this.resultsHandler();
   }
 
   render(): JSX.Element {
