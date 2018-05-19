a2a22e6055649aaab02885eef20cb2b23d1b9823
Daniel Deutsch <deudan1010@gmail.com>
2018-05-19 15:23:45 +0200

    #6 Add python backend folder, install tensorflow and add tensorflow test file

---
+++ pythonBackend/test.py
@@ -0,0 +1,14 @@
+# import tensorflow
+import tensorflow as tf
+
+sess = tf.Session()
+
+# Verify we can print a String
+hello = tf.constant("Hello Tensorflow")
+print(sess.run(hello))
+
+# Perform some simple math
+a = tf.constant(20)
+b = tf.constant(22)
+print('a + b = {0}'.format(sess.run(a + b)))
+
